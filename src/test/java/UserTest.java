85061e0c269c89ef7d33ad6c2ac37636c3ef3c98
peachberry <peachberry0318@gmail.com>
2021-02-28 00:07:18 +0900

    Add UserTest for testGetDraw

---
+++ src/test/java/UserTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+
+    @Test
+    void testUserGetDraw() {
+        /*
+        User user = new User();
+        Deck deck = new Deck();
+
+        user.draw(deck);
+        Assertions
+        .assertEquals(user.cards.size(), 1);
+         */
+    }
+
+}
