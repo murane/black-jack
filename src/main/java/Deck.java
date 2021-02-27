803d25b3c0fdc58bd28c579f2103f0ab1c854a7c
HJ-Woo <hjwoou@gmail.com>
2021-02-28 00:08:53 +0900

    feat(Deck): generateCards

---
+++ src/main/java/Deck.java
@@ -0,0 +1,27 @@
+public class Deck {
+    private Cards cards = new Cards();
+
+    public Deck() {
+        String[] suits = { "Heart", "Spade", "Diamond","Clova"};
+        for(String suit : suits){
+            generateNumberOf(suit);
+        }
+        cards.shuffle();
+    }
+
+    private void generateNumberOf(String suit){
+        String[] numbers = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+        for(String number : numbers){
+            Card card = new Card(suit, number);
+            cards.add(card);
+        }
+    }
+
+    public Cards getCards() {
+        return cards;
+    }
+
+    public Card pop() {
+        return cards.pop();
+    }
+}
---
+++ src/test/java/DeckTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DeckTest {
+    Deck deck;
+
+    //TODO 1. 카드 52장을 생성 및 셔플
+    @BeforeEach
+    void 덱생성(){
+        deck = new Deck();
+//      assertThat(deck.getCards().getSize()).isEqualTo(52);
+    }
+
+    //TODO 2.pop 카드 한장 꺼내가기
+    @Test
+    void 덱에서꺼내가기(){
+        deck.pop();
+        assertThat(deck.getCards().getSize()).isEqualTo(51);
+    }
+}
