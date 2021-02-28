e66a68b2367afee510a8552adee02c784e720d42
peachberry <peachberry0318@gmail.com>
2021-03-01 01:19:39 +0900

    feat(Calculator) :점수 계산을 위한 Calculator 구현

---
+++ src/main/java/util/Calculator.java
@@ -0,0 +1,27 @@
+package util;
+
+import card.Card;
+
+import java.util.List;
+
+public class Calculator {
+
+    public static int sumCardPoint(List<Card> cards) {
+        int sum = 0;
+        for (Card card : cards) {
+            sum += convert(card.getCardNumber());
+        }
+        return sum;
+    }
+
+    private static int convert(String cardNumber) {
+        if (cardNumber.equals("A")) {
+            return 1;
+        }
+        if (cardNumber.equals("J") || cardNumber.equals("Q") || cardNumber.equals("K")) {
+            return 10;
+        }
+        return Integer.parseInt(cardNumber);
+    }
+
+}
