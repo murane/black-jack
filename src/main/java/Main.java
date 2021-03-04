e14f9be7318c366aea604e07d24fabddcddf494f
peachberry <peachberry0318@gmail.com>
2021-03-04 22:44:56 +0900

    Refactor(Main) :Main에 Game 초기화 할때 User의존성 주입

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,15 @@
 import card.DeckFactory;
 import policy.WinnerPolicyImpl;
+import user.UserFactory;
 
 public class Main {
 
     public static void main(String[] args) {
         Game game = new Game(
                 new WinnerPolicyImpl(),
-                DeckFactory.getDefaultDeck()
+                DeckFactory.getDefaultDeck(),
+                UserFactory.getDealer(),
+                UserFactory.getGamer()
         );
         game.run();
     }
