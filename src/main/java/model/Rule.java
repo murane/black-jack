33717fa3d6d086701593d675b294c8abed68524f
Junsu <murane@naver.com>
2021-03-06 07:28:28 +0900

    Rule로 상수 분리

---
+++ src/main/java/model/Rule.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Rule {
+
+    public static final String DEALER_CARDS = "Dealer's cards: ";
+    public static final String GAMER_CARDS = "My cards: ";
+    public static final String BUST_CASE = "Bust!";
+
+    public static final String WIN = "WIN!";
+    public static final String LOSE = "LOSE";
+    public static final String DRAW = "DRAW!";
+
+    public static final String HIT = "h";
+    public static final String STAND = "s";
+
+    public static final int BLACKJACK = 21;
+    public static final int DEALER_RULE = 16;
+}
